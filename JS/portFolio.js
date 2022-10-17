ef1368d85c0d981414074f7d985c3c0012f4dc1c
666 MEMENTO <queen1448@nate.com>
2022-10-18 00:31:13 +0900

    save

--- JS/portFolio.js
+++ JS/portFolio.js
@@ -1,13 +1,13 @@
-let portFolio1 = document.getElementById("portfolio_detail_01");
+// let portFolio1 = document.getElementById("portfolio_detail_01");
 
-window.addEventListener("scroll", function (){
-  let value = window.scrollY
-  console.log("scrollY",value);
+// window.addEventListener("scroll", function (){
+//   let value = window.scrollY
+//   console.log("scrollY",value);
 
-  if(value > 1500) {
-    portFolio1.style.animation = "disAppear 1s ease-out forwards";
-  }else {
-    portFolio1.style.animation = `slide 1s ease-out`;
-  }
+//   if(value > 1500) {
+//     portFolio1.style.animation = "disAppear 1s ease-out forwards";
+//   }else {
+//     portFolio1.style.animation = `slide 1s ease-out`;
+//   }
   
-});
+// });
