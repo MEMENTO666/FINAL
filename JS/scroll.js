fd9f25c4941e85609c347e10208982a365b1c297
666 MEMENTO <queen1448@nate.com>
2022-11-19 17:54:28 +0900

    save

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,41 +1,43 @@
-window.onload = function(){
-  const elm = document.querySelectorAll('.section');
+window.onload = function () {
+  const elm = document.querySelectorAll(".section");
   const elmCount = elm.length;
-  elm.forEach(function(item, index){
-    item.addEventListener('mousewheel', function(event){
+  elm.forEach(function (item, index) {
+    item.addEventListener("mousewheel", function (event) {
       event.preventDefault();
       let delta = 0;
 
       if (!event) event = window.event;
       if (event.wheelDelta) {
-          delta = event.wheelDelta / 120;
-          if (window.opera) delta = -delta;
-      } 
-      else if (event.detail)
-          delta = -event.detail / 4;
+        delta = event.wheelDelta / 120;
+        if (window.opera) delta = -delta;
+      } else if (event.detail) delta = -event.detail / 4;
 
       let moveTop = window.scrollY;
       let elmSelector = elm[index];
 
       // wheel down : move to next section
-      if (delta < 0){
-        if (elmSelector !== elmCount-1){
-          try{
-            moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
-          }catch(e){}
-        }
-      }
-      
-      // wheel up : move to previous section
-      else{
-        if (elmSelector !== 0){
-          try{
-            moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
-          }catch(e){}
+      if (delta < 0) {
+        if (elmSelector !== elmCount - 1) {
+          try {
+            moveTop =
+              window.pageYOffset +
+              elmSelector.nextElementSibling.getBoundingClientRect().top;
+          } catch (e) {}
         }
       }
 
-      window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+      // wheel up : move to previous section
+      else {
+        if (elmSelector !== 0) {
+          try {
+            moveTop =
+              window.pageYOffset +
+              elmSelector.previousElementSibling.getBoundingClientRect().top;
+          } catch (e) {}
+        }
+      }
+
+      window.scrollTo({ top: moveTop, left: 0, behavior: "smooth" });
     });
   });
-}
+};
--- JS/topbutton.js
+++ JS/topbutton.js
@@ -1,24 +1,21 @@
-const TBTN = document.getElementById('topbutton');
+const TBTN = document.getElementById("topbutton");
 
-const checkScroll=()=>{
+const checkScroll = () => {
+  let pageOffset = window.pageYOffset;
 
-    let pageOffset = window.pageYOffset;
+  if (pageYOffset !== 0) {
+    TBTN.classList.add("show");
+  } else {
+    TBTN.classList.remove("show");
+  }
+};
 
-    if(pageYOffset !== 0){
+const moveBackToTop = () => {
+  if (window.pageYOffset > 0) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+};
 
-        TBTN.classList.add('show');  
-    }else{
-        TBTN.classList.remove('show'); 
-    }
-}
+window.addEventListener("scroll", checkScroll);
 
-const moveBackToTop=()=>{
-    if(window.pageYOffset > 0 ){
-        window.scrollTo({top:0, behavior:"smooth"});
-    }
-}
-
-
-window.addEventListener('scroll', checkScroll);
-
-TBTN.addEventListener('click',moveBackToTop);
+TBTN.addEventListener("click", moveBackToTop);
